eb4ddc9809909c6ec7e706e7da8a97f1ba23f220
fenris123 <fenris1234@hotmail.com>
2015-04-19 15:24:26 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+######PROGRAMING ASIGNMENT 2
 
-## Write a short comment describing this function
+
+####                   WARNING                           #####
+####                   WARNING                           #####
+
+####    Be sure that your matrix has an inverse.         #####
+####    Otherwhise,the result will be an error message   #####
+
+
+### This Function takes  a matrix, and fixed it's inverse it into the parent enviroment
+### if we need this value again and it has not changed
 
 makeCacheMatrix <- function(x = matrix()) {
+     
+         
+         
+     storematrix <<- x 
+     storeinverse <<- solve(x)
+     message ("storing the following inverse matrix")
+     print (solve(x))
 
 }
 
+## this seccond function creates a function that chek if the inverse of the matrix has been
+## solved before, and on this case takes this solution from the cache. If not, it just calculates it
+### and print the solution.
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+     if ( identical ((x),storematrix) == TRUE ){
+          message("getting cached data")
+          return(storeinverse)
+     }
+     
+         else {
+               message("calculating data")
+               return (solve(x))
+          }
+    
+     
 }
+   
